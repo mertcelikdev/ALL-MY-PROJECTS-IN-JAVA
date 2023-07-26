7578da99bd8dee13a84f3898136c9cbc39251baa
Mert Çelik <109079231+mrcelikk99@users.noreply.github.com>
2023-07-26 16:57:07 +0300

    Add files via upload
    Girdiğiniz değere en yakın değerleri veren bir program. Liste kendiliğinden hazır bir şekilde. Listenin dışına çok çıkarsanız değeri 0 verecektir.

---
+++ MaxMinDeğerler.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+
+public class MaxMinDeğerler {
+    public static void main(String[] args) throws Exception {
+        int [] list = {56, 34, 1, 8, 101, -2, -33};
+        // -33 -2 1 8 34 56 101
+        Arrays.sort(list);
+        int minClosed = 0;
+        int maxClosed = 0;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Değer giriniz: ");
+        int value = sc.nextInt();
+
+        for (int i : list) {
+            if (i < value ) {
+                minClosed = i;
+                break;
+            }
+        }
+
+        for (int i : list) {
+            if (i > value) {
+                maxClosed = i;
+                break;
+            }
+        }
+
+        System.out.println("En yakın minimum değer: " + minClosed);
+        System.out.println("En yakın maksimum değer: " + maxClosed);
+    }
+}
