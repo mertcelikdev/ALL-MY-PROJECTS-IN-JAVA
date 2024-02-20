fb03e6a453bc722e9b8c6602b0b9a734249c3711
Mert Çelik <109079231+mertcelikdev@users.noreply.github.com>
2024-02-20 15:50:18 +0300

    Kitap Listeleme

--- Book.java
+++ Book.java
@@ -1,107 +1,86 @@
-package Java102;
+package BookLister;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
- class Book implements Comparable<Book> {
+public class Book {
+    private String bookName;
+    private int pageNumber;
+    private String publisherName;
+    private String publishDate;
 
-     private String bookName;
-     private int pageNumber;
-     private String publisherName;
-     private String publishDate;
+    public Book(String bookName, int pageNumber, String publisherName, String publishDate) {
+        this.bookName = bookName;
+        this.pageNumber = pageNumber;
+        this.publisherName = publisherName;
+        this.publishDate = publishDate;
+    }
 
-     public Book(String bookName, int pageNumber, String publisherName, String publishDate) {
-         this.bookName = bookName;
-         this.pageNumber = pageNumber;
-         this.publisherName = publisherName;
-         this.publishDate = publishDate;
-     }
+    public String getBookName() {
+        return bookName;
+    }
 
-     public String getBookName() {
-         return bookName;
-     }
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
 
-     public void setBookName(String bookName) {
-         this.bookName = bookName;
-     }
+    public int getPageNumber() {
+        return pageNumber;
+    }
 
-     public int getPageNumber() {
-         return pageNumber;
-     }
+    public void setPageNumber(int pageNumber) {
+        this.pageNumber = pageNumber;
+    }
 
-     public void setPageNumber(int pageNumber) {
-         this.pageNumber = pageNumber;
-     }
+    public String getPublisherName() {
+        return publisherName;
+    }
 
-     public String getPublisherName() {
-         return publisherName;
-     }
+    public void setPublisherName(String publisherName) {
+        this.publisherName = publisherName;
+    }
 
-     public void setPublisherName(String publisherName) {
-         this.publisherName = publisherName;
-     }
+    public String getPublishDate() {
+        return publishDate;
+    }
 
-     public String getPublishDate() {
-         return publishDate;
-     }
+    public void setPublishDate(String publishDate) {
+        this.publishDate = publishDate;
+    }
 
-     public void setPublishDate(String publishDate) {
-         this.publishDate = publishDate;
-     }
+    public static void main(String[] args) {
+        ArrayList<Book> list = new ArrayList<>();
+        Book book1 = new Book("A", 100, "Mert", "10.10.2000");
+        Book book2 = new Book("B", 200, "John", "12.12.2005");
+        Book book3 = new Book("C", 150, "Alice", "05.05.2010");
+        Book book4 = new Book("D", 180, "Bob", "07.07.2015");
+        Book book5 = new Book("E", 120, "Emma", "09.09.2020");
+        Book book6 = new Book("F", 220, "David", "11.11.2023");
+        Book book7 = new Book("G", 90, "Sophia", "03.03.2018");
+        Book book8 = new Book("H", 130, "Oliver", "04.04.2019");
+        Book book9 = new Book("I", 110, "Sophie", "06.06.2017");
+        Book book10 = new Book("J", 170, "William", "08.08.2016");
 
-     @Override
-     public int compareTo(Book other) {
-         return this.bookName.compareTo(other.bookName);
-     }
+        list.add(book1);
+        list.add(book2);
+        list.add(book3);
+        list.add(book4);
+        list.add(book5);
+        list.add(book6);
+        list.add(book7);
+        list.add(book8);
+        list.add(book9);
+        list.add(book10);
 
+        Map<String, String> bookAuthorMap = list.stream()
+                .collect(Collectors.toMap(Book::getBookName, Book::getPublisherName));
 
-     public static void main(String[] args) {
-         Book book1 = new Book("Book1", 200, "Author1", "10.10.2000");
-         Book book2 = new Book("Book2", 150, "Author2", "11.10.2000");
-         Book book3 = new Book("Book3", 300, "Author3", "12.10.2000");
-         Book book4 = new Book("Book4", 180, "Author4", "13.10.2000");
-         Book book5 = new Book("Book5", 250, "Author5", "14.10.2000");
-
-
-         Set<Book> bookSetByName = new TreeSet<>();
-         bookSetByName.add(book1);
-         bookSetByName.add(book2);
-         bookSetByName.add(book3);
-         bookSetByName.add(book4);
-         bookSetByName.add(book5);
-
-         System.out.println("Books sorted by name:");
-         for (Book book : bookSetByName) {
-             System.out.println(book.getBookName());
-         }
-         System.out.println("-------------------------------------------------------------------------------");
-         System.out.println("-------------------------------------------------------------------------------");
-
-
-         Set<Book> bookSetByPageNumber = new TreeSet<>(new Comparator<Book>() {
-             @Override
-             public int compare(Book o1, Book o2) {
-                 return o1.getPageNumber() - o2.getPageNumber();
-             }
-         });
-
-
-         bookSetByPageNumber.add(book1);
-         bookSetByPageNumber.add(book2);
-         bookSetByPageNumber.add(book3);
-         bookSetByPageNumber.add(book4);
-         bookSetByPageNumber.add(book5);
-
-         System.out.println("Books sorted by page number:");
-         for (Book book : bookSetByPageNumber) {
-             System.out.println(book.getPageNumber());
-         }
-
-
-
-
-
-
-
-     }
- }
-
+        bookAuthorMap.forEach((book, author) -> System.out.println("Book: " + book + ", Author: " + author));
+        System.out.println("##########################################");
+        list.stream()
+                .filter(book -> book.getPageNumber() > 100)
+                .forEach(book -> System.out.println(book.getPageNumber()));
+    }
+}
